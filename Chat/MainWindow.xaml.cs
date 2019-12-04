209eda0d30bd6b43bce5fc6313677caca8d2c18e
Remhad <remi-tt@mail.de>
2019-12-04 14:00:18 +0100

    ChatServer project created

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Chat
 {
@@ -23,6 +13,25 @@ namespace Chat
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void ConnectButton_Click(object sender, RoutedEventArgs e)
+		{
+
+			try
+			{
+				var ipAddress = IPAddress.Parse(IpAdressInput.Text);
+				var client = new TcpClient();
+				client.Connect(ipAddress, 5000);
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Ungültige IP-Adresse");
+			}
+			catch (SocketException)
+			{
+				MessageBox.Show("Server nicht erreichbar");
+			}
 		}
 	}
 }
